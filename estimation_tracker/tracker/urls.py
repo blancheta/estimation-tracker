b26fc3f2b62959bd8b43a0c24a686c73db1e855a
markomijaljevic <markomijaljevic96@gmail.com>
2022-05-03 11:43:47 +0200

    switch to class based views

--- estimation_tracker/tracker/urls.py
+++ estimation_tracker/tracker/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('create_task', views.create_task, name='create_task'),
+    path("", views.HomeView.as_view(), name="home"),
+    path("create_task", views.FormView.as_view(), name="create_task"),
 ]
--- estimation_tracker/tracker/views.py
+++ estimation_tracker/tracker/views.py
@@ -1,28 +1,34 @@
 from django.shortcuts import redirect, render
+from django.views.generic import View
 from tracker.forms import TaskForm
 from tracker.models import Task
+
+class HomeView(View):
+    template_name = 'home.html'
     
-def home(request):
-    
-    context = {
-        'tasks': Task.objects.all()
-    }
-    
-    return render(request, 'home.html', context)
+    def get(self, request):
+        context = {
+            'tasks': Task.objects.all()
+        }
+        return render(request, self.template_name, context)
 
 
-def create_task(request):
+class FormView(View):
+    form_class = TaskForm
+    template_name = 'create_task.html'
     
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
+    def get(self, request, *args, **kwargs):            
+        context = {
+            'form' : self.form_class
+        }
+        return render(request, self.template_name, context)
+    
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
         
         if form.is_valid():
             form.save()
-
-        return redirect('home')
-    
-    context = {
-        'form' : TaskForm()
-    }
-    
-    return render(request, 'create_task.html', context)
+            return redirect('home')
+        
+        context = {'form': form}
+        return render(request, self.template_name, context)
