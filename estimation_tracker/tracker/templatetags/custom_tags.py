238adfd48214e56a21f6284ce60c1736dbc88c49
markomijaljevic <markomijaljevic96@gmail.com>
2022-05-03 18:16:19 +0200

    add docstring to custom tags functions

--- estimation_tracker/tracker/templatetags/custom_tags.py
+++ estimation_tracker/tracker/templatetags/custom_tags.py
@@ -6,6 +6,15 @@ register = template.Library()
 
 
 def convert_time_to_seconds(time: datetime.time) -> float:
+    """Convert datetime.time object to seconds.
+
+    Args:
+        time (datetime.time): Object containing time info.
+
+    Returns:
+        float: Total number of seconds.
+    """
+    
     seconds = datetime.timedelta(
         hours=time.hour, minutes=time.minute, seconds=time.second
     ).total_seconds()
@@ -16,6 +25,15 @@ def convert_time_to_seconds(time: datetime.time) -> float:
 def calculate_correctness_ratio(
     task_real_time: datetime.time, task_self_estimated_time: datetime.time
 ) -> int:
+    """Percentage %, Ratio of real time over the estimated time
+
+    Args:
+        task_real_time (datetime.time): Time when task was finished.
+        task_self_estimated_time (datetime.time): Estimated task finish time.
+
+    Returns:
+        int: Calculated task estimation accuracy (percentage).
+    """
 
     real_time_seconds = convert_time_to_seconds(task_real_time)
     estimated_time_seconds = convert_time_to_seconds(task_self_estimated_time)
@@ -26,6 +44,16 @@ def calculate_correctness_ratio(
 def estimate_time(
     task_real_time: datetime.time, task_self_estimated_time: datetime.time
 ) -> datetime.time:
+    """Estimated time for subsequent tasks using Correctness
+    and Self estimated time to predict the estimated time.
+
+    Args:
+        task_real_time (datetime.time): Time when task was finished.
+        task_self_estimated_time (datetime.time): Estimated task finish time.
+
+    Returns:
+        datetime.time: Auto calculated estimation time.
+    """
 
     correctness_ratio = calculate_correctness_ratio(
         task_real_time, task_self_estimated_time
