6b72bf121ae00e7fe54f30a7dd2bafa7b1d45017
markomijaljevic <markomijaljevic96@gmail.com>
2022-05-03 12:52:53 +0200

    make real_time_span field optional

--- estimation_tracker/tracker/models.py
+++ estimation_tracker/tracker/models.py
@@ -6,7 +6,7 @@ class Task(models.Model):
     task_name = models.TextField(blank=False)
     planning_time = models.TimeField(blank=False)
     self_estimated_time = models.TimeField(blank=False)
-    real_time_spent = models.TimeField(blank=False)
+    real_time_spent = models.TimeField(blank=True)
     
     notes = models.TextField(blank=True)
     level = models.CharField(max_length=64, blank=True)
