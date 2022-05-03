5eadece486a8ab1f27b00f883b0ba3e9a11f98f9
markomijaljevic <markomijaljevic96@gmail.com>
2022-05-03 12:12:42 +0200

    move everything to one class view

--- estimation_tracker/tracker/urls.py
+++ estimation_tracker/tracker/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.HomeView.as_view(), name="home"),
-    path("create_task", views.FormView.as_view(), name="create_task"),
+    path("tasks", views.TasksHomeView.as_view(), name="home"),
 ]
--- estimation_tracker/tracker/views.py
+++ estimation_tracker/tracker/views.py
@@ -1,34 +1,24 @@
+from audioop import reverse
 from django.shortcuts import redirect, render
 from django.views.generic import View
 from tracker.forms import TaskForm
 from tracker.models import Task
 
-class HomeView(View):
-    template_name = 'home.html'
-    
-    def get(self, request):
-        context = {
-            'tasks': Task.objects.all()
-        }
-        return render(request, self.template_name, context)
 
-
-class FormView(View):
+class TasksHomeView(View):
+    template_name = "home.html"
     form_class = TaskForm
-    template_name = 'create_task.html'
-    
-    def get(self, request, *args, **kwargs):            
-        context = {
-            'form' : self.form_class
-        }
+
+    def get(self, request):
+        context = {"tasks": Task.objects.all(), "form": self.form_class()}
         return render(request, self.template_name, context)
-    
+
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
-        
+
         if form.is_valid():
             form.save()
             return redirect('home')
         
-        context = {'form': form}
+        context = {"tasks": Task.objects.all(), "form": self.form_class()}
         return render(request, self.template_name, context)
