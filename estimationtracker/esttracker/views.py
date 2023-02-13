4dfca7ff64d03ef38be775429ee5b7acd2e475e2
Joanna <asia.jeziorekk@gmail.com>
2023-02-13 09:45:14 +0100

    multi-inheritance in view

--- estimationtracker/esttracker/views.py
+++ estimationtracker/esttracker/views.py
@@ -1,17 +1,16 @@
-from django.shortcuts import render
-from .models import Task
-from .forms import CreateTask
-from django.views.generic import  CreateView
 from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.edit import BaseCreateView
+
+from .forms import CreateTask
+from .models import Task
 
 
-class TaskCreateListView(CreateView):
+class TaskCreateListView(BaseCreateView, ListView):
     model = Task
-    template_name = 'task_create_list.html'
+    template_name = "task_create_list.html"
     form_class = CreateTask
-    success_url = reverse_lazy('esttracker:task_create_list')
+    success_url = reverse_lazy("esttracker:task_create_list")
 
-    def get_context_data(self, **kwargs):
-        context = super(TaskCreateListView, self).get_context_data(**kwargs)
-        context["tasks"] = Task.objects.all().order_by('created_at')
-        return context
+    def get_queryset(self):
+        return Task.objects.all().order_by("created_at")
