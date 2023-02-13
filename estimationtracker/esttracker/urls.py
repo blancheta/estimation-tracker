05e9076f8432888f7e7f7dac2240447f9a8b9f03
Joanna <asia.jeziorekk@gmail.com>
2023-02-13 09:49:44 +0100

    remove app_name

--- estimationtracker/esttracker/urls.py
+++ estimationtracker/esttracker/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 
 from . import views
 
-app_name = "esttracker"
-
 urlpatterns = [
     path("", views.TaskCreateListView.as_view(), name="task_create_list"),
 ]
--- estimationtracker/esttracker/views.py
+++ estimationtracker/esttracker/views.py
@@ -10,7 +10,7 @@ class TaskCreateListView(BaseCreateView, ListView):
     model = Task
     template_name = "task_create_list.html"
     form_class = CreateTask
-    success_url = reverse_lazy("esttracker:task_create_list")
+    success_url = reverse_lazy("task_create_list")
 
     def get_queryset(self):
         return Task.objects.all().order_by("created_at")
